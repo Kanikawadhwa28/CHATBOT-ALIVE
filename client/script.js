775ea9d01fd2c6c35c3f01f0eb5b8201c4881bef
Kanikawadhwa28 <kanika.28.w@gmail.com>
2023-03-15 16:52:38 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
- const response = await fetch('https://codings.onrender.com', {
+ const response = await fetch('http://localhost:5195', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
